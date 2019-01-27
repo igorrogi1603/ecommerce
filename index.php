89efaaa9a8267d4e0a24b918df4ebd0bda3b2494
IGOR VIEIRA DOS SANTOS <igor_rogi_16032000@uninove.edu.br>
2019-01-27 16:38:36 -0200

    Admin Categories

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new \Slim\Slim();
 
@@ -115,7 +116,7 @@ $app->get("/admin/users/:iduser", function($iduser) {
 $app->post("/admin/users/create", function() {
 
 	//ESSE CODIGO USAR QUANDO TIVER CRIPTOGRAFIA
-	/*User::verifyLogin();
+	User::verifyLogin();
 
 	$user = new User();
 
@@ -127,10 +128,10 @@ $app->post("/admin/users/create", function() {
 	$user->save();
 
 	header("Location: /admin/users");
-	exit;*/
+	exit;
 
 	//USAR ESSE POR ENQUANTO
-	User::verifyLogin();
+	/*User::verifyLogin();
 
 	$user = new User();
 
@@ -147,7 +148,7 @@ $app->post("/admin/users/create", function() {
 	$user->save();
 
 	header("Location: /admin/users");
- 	exit;
+ 	exit;*/
 
 });
 
@@ -167,6 +168,163 @@ $app->post("/admin/users/:iduser", function($iduser) {
 
 	header("Location: /admin/users");
 	exit; 
+
+});
+
+$app->get("/admin/forgot", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get("/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);	
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, ["cost"=>12]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategor", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategor", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer {
+	//-----------------------------------------------------------------------//
+	//																		 //
+	//	  USAR UM GMAIL JA EXISTENTE PARA ENVIAR OS EMAIL DE RECUPERAÇÃO     //
+	//																		 //
+	//-----------------------------------------------------------------------//
+	const USERNAME = "seugmail@gmail.com";
+	const PASSWORD = "sua senha";
+	const NAME_FROM = "Hcode Store";
+
+	private $mail;
+
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+	{
+
+		$config = array(
+			"tpl_dir"	=> $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"	=> $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"		=> false
+		);
+		
+		Tpl::configure( $config );
+		
+		$tpl = new Tpl;
+
+		foreach ($data as $key => $value) {
+			$tpl->assign($key, $value);
+		}
+
+		$html = $tpl->draw($tplName, true);
+
+		$this->mail = new \PHPMailer;
+
+		//Enable SMTP debugging
+		//0 = production
+		//1 = client menssages
+		//2 = clint and server messages
+		$this->mail->SMTPDebug = 0;
+
+		$this->mail->Debugoutput = 'html';
+
+		$this->mail->Host = 'smtp.gmail.com';
+
+		$this->mail->Port = 587;
+
+		//ativar apenas para teste 
+		//nao deixar ativado pois deixa o email vuneravel
+		//ele permite enviar o emil com http 
+		//pois o google nao deixa receber pois estamos usando http nao https
+		$this->mail->isSMTP();
+		$this->mail->SMTPOptions = array(
+		    'ssl' => array(
+		        'verify_peer' => false,
+		        'verify_peer_name' => false,
+		        'allow_self_signed' => true
+		    )
+		);
+
+		$this->mail->SMTPSecure = 'tls';
+
+		$this->mail->SMTPAuth = true;
+
+		$this->mail->Username = Mailer::USERNAME;
+
+		$this->mail->Password = Mailer::PASSWORD;
+
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+
+		$this->mail->addAddress($toAddress, $toName);
+
+		$this->mail->Subject = $subject;
+
+		$this->mail->msgHTML($html);
+
+		$this->mail->AltBody = 'Mensagem que vai aparecer';
+
+	}
+
+	public function send()
+	{
+		return $this->mail->send();
+	}
+
+}
+
+?>
